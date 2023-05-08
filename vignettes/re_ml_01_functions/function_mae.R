e29a8ea83a286000343ef32f4583507f83672a7a
Naomi Huber <naomihuber@macbook-pro-von-naomi.home>
2023-05-08 22:56:45 +0200

    upload of the function_mae

---
+++ vignettes/re_ml_01_functions/function_mae.R
@@ -0,0 +1,13 @@
+mae <- test_mae <- numeric(length(k_values))
+for (k in k_values) {
+  # fit the model on training data
+  model <- train(VPD_F ~ ., method = "knn", trControl = trainControl(method = "none"), tuneGrid = data.frame(k = k), data = train)
+  
+  #evaluation of the training data
+  y_train_predicted <- predict(model, newdata = train)
+  mae[k] <- mean(abs(y_train_predicted - train$VPD_F))
+  
+  #evaluation of the test data
+  y_test_predicted <- predict(model, newdata = test)
+  mae[k] <- mean(abs(y_test_predicted - test$VPD_F))
+}
